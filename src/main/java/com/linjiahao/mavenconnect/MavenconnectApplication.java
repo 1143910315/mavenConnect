d7a902c439971ee6169d802da45f919dae440f90
HapiFive <1143910315@qq.com>
2021-01-27 19:57:05 +0800

    Initial commit

---
+++ src/main/java/com/linjiahao/mavenconnect/MavenconnectApplication.java
@@ -0,0 +1,13 @@
+package com.linjiahao.mavenconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenconnectApplication.class, args);
+    }
+
+}
